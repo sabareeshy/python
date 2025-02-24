7d7fcba4d122b8db6a1ea5a4d5be9c7e67481d80
sabareesh yarru <sabareeshy@gmail.com>
2025-02-24 17:28:15 +0530

    added code

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def subtraction():
+    sub = num1 - num2
+    print(sub)
+
+def multiplication():
+    mul = num1 * num2
+    print(mul)
+
+addition()
+subtraction()
+multiplication()
+
